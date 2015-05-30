bba8ec0090cb6495ede648d43a3e5fd347758182
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2015-05-30 11:34:03 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pydatawrapper",
+    version="0.1",
+    description='python wrapper library for datawrapper',
+    author='Ara Hayrabedian',
+    author_email='ara.hayrabedian@gmail.com',
+    install_requires=[
+        "requests",
+        "marshmallow",
+    ],
+)
